29ea9c70ed99ca843a7b1906639b0ecacaa13a4c
Rahul Rajish Nathan <74354544+rahulrajish@users.noreply.github.com>
2023-08-22 22:45:33 +0530

    Adding changes to refactor code
    1.) Added separate structs for Echo and Cat
    2.) Created Config trait for the Echo and Cat methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,119 @@
 use std::fs;
-
-pub struct Config {
-    pub input_type: String,
-    pub query_1: String,
-    pub query_2: Option<String>,
+use std::error::Error;
+pub trait Config{
+    fn build(inputs: Vec<String>) -> Result<Box<Self>, &'static str>;
+    fn run(&self) -> Result<(), Box<dyn Error>>;
 }
 
-impl Config {
-    pub fn build(mut args: impl Iterator<Item = String>,) -> Result<Config, &'static str> {
-        args.next();
+pub struct EchoConfig {
+    pub input: String,
+}
 
-        let input_type = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get an input string"),
+pub struct CatConfig {
+    pub file_path_1: String,
+    pub file_path_2: String,
+}
+
+impl Config for EchoConfig {
+
+    fn build(inputs: Vec<String>) -> Result<Box<Self>, &'static str> {
+        let input = match inputs.into_iter().next() {
+            Some(input) => input,
+            None => return Err("Didn't get input string"),
         };
+        Ok(Box::new(EchoConfig { input }))
+    }
 
-        let query_1 = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a query string"),
-        };
+    fn run(&self) -> Result<(), Box<dyn Error>> {
+        println!("{0}", self.input);
 
-        let query_2 = args.next();
-
-        //let file_name_2 = args.next();
-
-        Ok(Config { input_type, query_1, query_2})
-
+        Ok(())
     }
 }
 
-pub fn run(config: Config) -> Result<(), &'static str> {
-    let input_type = &config.input_type[..];
-    match input_type {
-        "echo" => println!("{0}", config.query_1),
+impl Config for CatConfig {
+
+    fn build(inputs: Vec<String>) ->  Result<Box<Self>, &'static str>{
+        let mut inputs_iter = inputs.into_iter();
+        let file_path_1 = match inputs_iter.next() {
+            Some(input) => input,
+            None => return Err("Didn't get input string"),
+        };
+
+        let file_path_2 = match inputs_iter.next() {
+            Some(input) => input,
+            None => return Err("Didn't get input string"),
+        };
+
+        Ok(Box::new(CatConfig{file_path_1, file_path_2}))
+    }
+
+    fn run(&self) -> Result<(), Box<dyn Error>> {
+        let file_name_1 = match fs::read_to_string(&self.file_path_1) {
+            Ok(file_path) => file_path,
+            Err(e) => return Err(Box::new(e)),
+        };
+
+        let file_name_2 = match fs::read_to_string(&self.file_path_2) {
+            Ok(file_path) => file_path,
+            Err(e) => return Err(Box::new(e)),
+        };
+
+        println!("{0}",file_name_1 + &file_name_2);
+
+        Ok(())
+    }
+}
+
+pub fn run_config(mut args: impl Iterator<Item = String>) -> Result<(), &'static str> {
+    args.next();
+
+    let config_type = match args.next() {
+        Some(arg) => arg,
+        None => return Err("Didn't get an input string"),
+    };
+
+    match config_type.as_str() {
+        "echo" => {
+            let mut inputs = Vec::new();
+            match args.next() {
+                Some(arg) => inputs.push(arg),
+                None => return Err("Didn't get an input string"),
+            }
+
+            let echo_config = EchoConfig::build(inputs);
+
+            let echo_run = match echo_config {
+                Ok(echo) => *echo,
+                Err(e) => return Err(e), 
+            };
+
+            echo_run.run().unwrap();
+        },
         "cat" => {
-            match concat(config) {
-                Ok(str) => println!("{str}"),
+            let mut inputs = Vec::new();
+            match args.next() {
+                Some(arg) => inputs.push(arg),
+                None => return Err("Didn't get an input string"),
+            }
+            match args.next() {
+                Some(arg) => inputs.push(arg),
+                None => return Err("Didn't get an input string"),
+            }
+
+            let cat_config = CatConfig::build(inputs);
+
+            let cat_run = match cat_config {
+                Ok(cat) => *cat,
                 Err(e) => return Err(e),
             };
+
+            cat_run.run().unwrap();
         }
-        _ => return Err("Unable to process input"),
+        _ => return Err("No operation found."),
     };
 
     Ok(())
-}
-
-fn concat(config: Config) -> Result<String, &'static str>{
-    let file_name_1 = config.query_1;
-    
-    let file_name_2 = match config.query_2 {
-        Some(file_name) => file_name,
-        None => return Err("Didn't get filePath string"),
-    };
-
-    let file_name_1 = match fs::read_to_string(file_name_1) {
-        Ok(file_path) => file_path,
-        Err(_) => return Err("Error while reading file from file path 1"),
-    };
-    let file_name_2 = match fs::read_to_string(file_name_2) {
-        Ok(file_path) => file_path,
-        Err(_) => return Err("Error while reading file from file path 2"),
-    };
-
-    Ok(file_name_1 + &file_name_2)
 
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
 use std::env;
 use std::process;
-use cliutils::Config;
+use cliutils::run_config;
 
 fn main() {
-    let config = Config::build(env::args()).unwrap_or_else(|err| {
+    run_config(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments : {err}");
-        process::exit(1);
-    });
-
-    cliutils::run(config).unwrap_or_else(|err|{
-        eprintln!("Problem while running command : {err}");
         process::exit(1);
     });
 }
